e255e938d71fc59eea131d1aae4f5bcd956e1503
weitinglin <weitinglin66@gmail.com>
2016-12-29 14:21:33 +0800

    build the basic shiny style with the navbar and markdown file

---
+++ DESCRIPTION.md
@@ -0,0 +1,14 @@
+# The impact of Cancer-associated fibroblast in primary non-small cell lung cancer stem cell   
+Lin, Ting-Wei
+
+## Biological Question
+1. What's the impact of Cancer-associated fibroblast on the cancer stem cell transcriptom?
+2. Which part of these impact determined the drug-resistant and migration characteristics in the lung cancer stem cell?
+3. What hypothesis can be generated for these phenomenon from our data (microarray, RNAseq, Proteomics)
+
+
+## Thinking on methology approach
+1. How you deal with your microarray data?
+2. How you deal with RNAseq data?
+3. How you deal with the Proteomic data?
+4. How you manage whole the project?
--- app.R
+++ app.R
@@ -4,35 +4,21 @@ library(VennDiagram)
 
 library(shiny)
 
-ui <- fluidPage( 
-    
-    sidebarLayout(
-        sidebarPanel(
-            fileInput(inputId = 'file1',label = 'choose file to upload',
-                      accept =c('text/csv',"text/comma-separated-values","text/plain")
-            )),
-        mainPanel(
-            dataTableOutput(outputId = "demo1")
+ui <- tagList( 
+   
+    navbarPage(title = "Project",
+        tabPanel(
+            "Introduction",
+            column(12,
+                   includeMarkdown("DESCRIPTION.md")
+                   )
         )
-    )
+    ) 
+   
 )
 
-source("/Users/Weitinglin/Documents/R_scripts/Lab/temple_shiny.R")
 server <- function(input, output){
-    
-    rawdata <- reactive({input$file1})
-    
-    output$demo1 <- renderDataTable({
-        if (is.null(rawdata()))
-            return(NULL)
-        
-        query  <- read.csv(rawdata()$datapath, header = FALSE, sep=" ")
-        result <- KEGG(query$V1)
-        result
-    })
-    
-    
-}
+    }
 
 
 
