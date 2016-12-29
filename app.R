d9f58f584082a4b4d8ef781b69a9f73cb3cbe174
weitinglin <weitinglin66@gmail.com>
2016-12-29 16:40:39 +0800

    compartization the code of app.R

--- app.R
+++ app.R
@@ -1,4 +1,14 @@
 library(shiny)
+source("/Users/Weitinglin/Documents/Repository/code_in_lab/00_microarry_function.R")
+load("/Users/Weitinglin/Documents/Repository/code_in_lab/ttresult.Rdata")
+
+
+# Preprocess --------------------------------------------------------------
+
+
+
+
+# UI part -----------------------------------------------------------------
 
 
 ui <- tagList( 
@@ -13,12 +23,25 @@ ui <- tagList(
         tabPanel(
             title = "Workflow",
             img(src="workflow.png")
+        ),
+        tabPanel(
+            title = "Gene",
+            dataTableOutput(outputId = "queryResult")
+        )
         )
     ) 
    
-)
+
+
+# SERVER part -------------------------------------------------------------
+
 
 server <- function(input, output){
+    output$queryResult <-  renderDataTable({
+        searchHarmonizome(c("CD44","ALDH1A3","CD9","CDKN2A","DPP4","HSPB1","KIT","NANOG"))
+    })
+    
+    
     }
 
 
