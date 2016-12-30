9385033287b74a15a195c5999f10b51d9027568d
weitinglin <weitinglin66@gmail.com>
2016-12-30 17:02:07 +0800

    finish the filter button in the result page

--- app.R
+++ app.R
@@ -7,45 +7,45 @@ load("/Users/Weitinglin/Documents/Repository/code_in_lab/total_probe_dataframe.R
 
 # Preprocess --------------------------------------------------------------
 
-# UP-regulation 
-
-
-t.greater.CLF_CLS <- t.greater.CLF_CLS %>% mutate(Case = "P6+fibroblast > P6 nofilter", Method = "Nofilter")
-t.greater.Sphere_CLF <- t.greater.Sphere_CLF %>% mutate(Case = "P6-Sphere > P6+fibroblast nofilter", Method = "Nofilter")
-t.greater.Sphere_CLS <- t.greater.Sphere_CLS %>% mutate(Case = "P6-Sphere > p6 nofilter", Method = "Nofilter")
-total.t.greater <- bind_rows(t.greater.CLF_CLS, t.greater.Sphere_CLF, t.greater.Sphere_CLS)
-
-
-# DOWN-regulation 
-t.less.CLF_CLS <- t.less.CLF_CLS %>% mutate(Case = "P6+fibroblast < P6 nofilter", Method = "Nofilter")
-t.less.Sphere_CLF <- t.less.Sphere_CLF %>% mutate(Case = "P6-Sphere < P6+fibroblast nofilter", Method = "Nofilter")
-t.less.Sphere_CLS <- t.less.Sphere_CLS %>% mutate(Case = "P6-Sphere < p6 nofilter", Method = "Nofilter")
-total.t.less <- bind_rows(t.less.CLF_CLS, t.less.Sphere_CLF, t.less.Sphere_CLS)
-
-# group_without_filter 
-total_without_filter <- bind_rows(total.t.greater, total.t.less)
-
-
-# UP-regulation 
-f.t.greater.CLF_CLS <- f.t.greater.CLF_CLS %>% mutate(Case = "P6+fibroblast > P6", Method = "Filter")
-f.t.greater.Sphere_CLF <- f.t.greater.Sphere_CLF %>% mutate(Case = "P6-Sphere > P6+fibroblast", Method = "Filter")
-f.t.greater.Sphere_CLS <- f.t.greater.Sphere_CLS %>% mutate(Case = "P6-Sphere > p6", Method = "Filter")
-total.f.t.greater <- bind_rows(f.t.greater.CLF_CLS, f.t.greater.Sphere_CLF, f.t.greater.Sphere_CLS)
-
-
-# DOWN-regulation 
-f.t.less.CLF_CLS <- f.t.less.CLF_CLS %>% mutate(Case = "P6+fibroblast < P6", Method = "Filter")
-f.t.less.Sphere_CLF <- f.t.less.Sphere_CLF %>% mutate(Case = "P6-Sphere < P6+fibroblast", Method = "Filter") 
-f.t.less.Sphere_CLS <- f.t.less.Sphere_CLS %>% mutate(Case = "P6-Sphere < p6", Method = "Filter") 
-total.f.t.less <- bind_rows(f.t.less.CLF_CLS, f.t.less.Sphere_CLF, f.t.less.Sphere_CLS)
-
-
-
-# group_with_filter 
-total_with_filter    <- bind_rows(total.f.t.greater, total.f.t.less)
-
-
-total_ttest_result <- bind_rows(total_without_filter, total_with_filter)
+# # UP-regulation 
+# 
+# 
+# t.greater.CLF_CLS <- t.greater.CLF_CLS %>% mutate(Case = "P6+fibroblast > P6", Method = "Nofilter")
+# t.greater.Sphere_CLF <- t.greater.Sphere_CLF %>% mutate(Case = "P6-Sphere > P6+fibroblast", Method = "Nofilter")
+# t.greater.Sphere_CLS <- t.greater.Sphere_CLS %>% mutate(Case = "P6-Sphere > p6", Method = "Nofilter")
+# total.t.greater <- bind_rows(t.greater.CLF_CLS, t.greater.Sphere_CLF, t.greater.Sphere_CLS)
+# 
+# 
+# # DOWN-regulation 
+# t.less.CLF_CLS <- t.less.CLF_CLS %>% mutate(Case = "P6+fibroblast < P6", Method = "Nofilter")
+# t.less.Sphere_CLF <- t.less.Sphere_CLF %>% mutate(Case = "P6-Sphere < P6+fibroblast", Method = "Nofilter")
+# t.less.Sphere_CLS <- t.less.Sphere_CLS %>% mutate(Case = "P6-Sphere < p6", Method = "Nofilter")
+# total.t.less <- bind_rows(t.less.CLF_CLS, t.less.Sphere_CLF, t.less.Sphere_CLS)
+# 
+# # group_without_filter 
+# total_without_filter <- bind_rows(total.t.greater, total.t.less)
+# 
+# 
+# # UP-regulation 
+# f.t.greater.CLF_CLS <- f.t.greater.CLF_CLS %>% mutate(Case = "P6+fibroblast > P6", Method = "Filter")
+# f.t.greater.Sphere_CLF <- f.t.greater.Sphere_CLF %>% mutate(Case = "P6-Sphere > P6+fibroblast", Method = "Filter")
+# f.t.greater.Sphere_CLS <- f.t.greater.Sphere_CLS %>% mutate(Case = "P6-Sphere > p6", Method = "Filter")
+# total.f.t.greater <- bind_rows(f.t.greater.CLF_CLS, f.t.greater.Sphere_CLF, f.t.greater.Sphere_CLS)
+# 
+# 
+# # DOWN-regulation 
+# f.t.less.CLF_CLS <- f.t.less.CLF_CLS %>% mutate(Case = "P6+fibroblast < P6", Method = "Filter")
+# f.t.less.Sphere_CLF <- f.t.less.Sphere_CLF %>% mutate(Case = "P6-Sphere < P6+fibroblast", Method = "Filter") 
+# f.t.less.Sphere_CLS <- f.t.less.Sphere_CLS %>% mutate(Case = "P6-Sphere < p6", Method = "Filter") 
+# total.f.t.less <- bind_rows(f.t.less.CLF_CLS, f.t.less.Sphere_CLF, f.t.less.Sphere_CLS)
+# 
+# 
+# 
+# # group_with_filter 
+# total_with_filter    <- bind_rows(total.f.t.greater, total.f.t.less)
+# 
+# 
+# total_ttest_result <- bind_rows(total_without_filter, total_with_filter)
 
 
 
@@ -71,8 +71,8 @@ ui <- tagList(
           fluidRow(
               column(1),
               column(11,selectInput(inputId = "Result.filter",
-                          label = "Filter:",
-                          choices = c("Nofilter" = "Nofilter","Filter"="Filter")))
+                          label = "Pre-filter before the t-test:",
+                          choices = c("Nofilter" = "Nofilter","Filter by variance at 50%"="Filter")))
           ),
           fluidRow(
               column(1),
@@ -94,6 +94,8 @@ server <- function(input, output){
     
     output$Result.table <- renderDataTable({
         total_ttest_result %>% mutate(A = 0.5*(estimate1 + estimate2)) %>% group_by(Case) %>% 
+            filter(Method == input$Result.filter) %>%
+            filter(!Case %in% c ("P6-Sphere > P6+fibroblast","P6-Sphere < P6+fibroblast", "P6-Sphere < P6+fibroblast nofilter", "P6-Sphere > P6+fibroblast nofilter")) %>% 
             summarise(n_0.05 = sum(adjusted.p < 0.05, na.rm = TRUE),
                       n_0.01 = sum(adjusted.p < 0.01, na.rm = TRUE),
                       n_0.001 = sum(adjusted.p < 0.001, na.rm = TRUE),
